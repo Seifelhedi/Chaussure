a156adbf0b6ad4d70514b65726c66df401f3f923
Seifelhedi <pmstartupe@gmail.com>
2025-03-11 10:36:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+let selectedPath = null;
+const paths = document.querySelectorAll(".product-shape");
+
+paths.forEach(path => {
+    path.addEventListener("click", function() {
+        if (selectedPath) {
+            selectedPath.classList.remove("selected");
+        }
+        selectedPath = this;
+        selectedPath.classList.add("selected");
+        const colorPickerContainer = document.getElementById("color-picker-container");
+        colorPickerContainer.style.display = "block";
+    });
+});
+
 function changeColorPicker(picker) {
     if (selectedPath) {
         const color = `#${picker.value}`;
         selectedPath.style.fill = color;
         selectedPath.style.stroke = "none";
 
-        // Afficher l'animation en plein écran
+        // Ajouter l'animation après le changement de couleur
         const animation = document.getElementById("color-animation");
         animation.classList.remove("hidden");
-        animation.style.display = "block"; // Afficher
+
+        // Positionner l'animation sur l'élément SVG sélectionné
+        const rect = selectedPath.getBoundingClientRect();
+        animation.style.position = "absolute";
+        animation.style.left = `${rect.left}px`;
+        animation.style.top = `${rect.top}px`;
+        animation.style.width = `${rect.width}px`;
+        animation.style.height = `${rect.height}px`;
+        animation.style.zIndex = "3";
 
         // Redémarrer et jouer l'animation
         animation.currentTime = 0;
         animation.play();
-
-        // Masquer l'animation après la fin
-        animation.onended = () => {
-            animation.style.display = "none";
-        };
     }
 }
