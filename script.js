7889662be6277795b08879a2266aabefe1e0b88f
Seifelhedi <pmstartupe@gmail.com>
2025-03-11 11:53:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
-
 let selectedPath = null;
 const paths = document.querySelectorAll(".product-shape");
-const animationCanvas = document.getElementById("animation-canvas");
-const lottie = new DotLottie({
-    autoplay: false,
-    loop: false,
-    canvas: animationCanvas,
-    src: "https://lottie.host/YOUR_ANIMATION_ID.lottie" // Remplace avec ton URL Lottie
-});
+
+// Ajouter un écouteur d'événement pour chaque path
 paths.forEach(path => {
     path.addEventListener("click", function() {
         // Supprimer la sélection de l'ancien path si un autre est sélectionné
         if (selectedPath) {
             selectedPath.classList.remove("selected");
         }
+        // Sélectionner le nouveau path
         selectedPath = this;
         selectedPath.classList.add("selected");
 
+        // Afficher le sélecteur de couleur
         const colorPickerContainer = document.getElementById("color-picker-container");
         colorPickerContainer.style.display = "block";
     });
@@ -28,16 +23,19 @@ function changeColorPicker(picker) {
         const color = `#${picker.value}`;
         selectedPath.style.fill = color;
         selectedPath.style.stroke = "none";
-        animationCanvas.classList.remove("hidden");
-        const rect = selectedPath.getBoundingClientRect();
-        const containerRect = document.getElementById("container").getBoundingClientRect();
-        animationCanvas.style.left = `${rect.left - containerRect.left}px`;
-        animationCanvas.style.top = `${rect.top - containerRect.top}px`;
-        animationCanvas.style.width = `${rect.width}px`;
-        animationCanvas.style.height = `${rect.height}px`;
-        lottie.play();
-        setTimeout(() => {
-            animationCanvas.classList.add("hidden");
-        }, 2000);
+
+
+        const video = document.getElementById("color-animation");
+        if (video.readyState >= 2) {
+            video.style.zIndex = "20";
+            video.style.display = "block";
+            video.currentTime = 0;
+
+            video.onended = () => {
+                video.style.display = "none";
+            };
+        } else {
+            console.log("Vidéo non prête, chargement en cours...");
+        }
     }
 }
