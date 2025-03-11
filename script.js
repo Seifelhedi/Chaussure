96d53ffa9552b9581dc5e488b354995562390bae
Seifelhedi <pmstartupe@gmail.com>
2025-03-11 10:25:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const paths = document.querySelectorAll(".product-shape");
 
 paths.forEach(path => {
     path.addEventListener("click", function() {
-
         if (selectedPath) {
             selectedPath.classList.remove("selected");
         }
@@ -19,8 +18,12 @@ function changeColorPicker(picker) {
         const color = `#${picker.value}`;
         selectedPath.style.fill = color;
         selectedPath.style.stroke = "none";
+
+        // Ajouter l'animation après le changement de couleur
         const animation = document.getElementById("color-animation");
         animation.classList.remove("hidden");
+
+        // Positionner l'animation sur l'élément SVG sélectionné
         const rect = selectedPath.getBoundingClientRect();
         animation.style.position = "absolute";
         animation.style.left = `${rect.left}px`;
@@ -28,6 +31,8 @@ function changeColorPicker(picker) {
         animation.style.width = `${rect.width}px`;
         animation.style.height = `${rect.height}px`;
         animation.style.zIndex = "3";
+
+        // Redémarrer et jouer l'animation
         animation.currentTime = 0;
         animation.play();
     }
